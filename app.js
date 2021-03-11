97675913519d776dde8c182a96350a04b0f6f366
JayTrairat <trirat.p@zenalyse.co.th>
2021-03-11 11:16:22 +0700

    feat: all posible socket

--- app.js
+++ app.js
@@ -9,20 +9,79 @@ const io = require("socket.io")(http, {
 var cors = require("cors");
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/notification", (req, res) => {
+  const status = [10, 11, 20, 30];
+  const index = Math.floor(Math.random() * 3);
   const mockedData = {
     noti_id: 1,
     mcid: 1,
     noti_date: new Date(),
-    current_state: 20,
+    current_state: status[index],
   };
   io.emit("status-1", mockedData);
   res.send("sended");
 });
 
-io.on("connection", (socket) => {
-  
+app.get("/status", (req, res) => {
+  const status = [10, 11, 20, 30];
+  const index = Math.floor(Math.random() * 3);
+  const mockedData = {
+    noti_id: 1,
+    mcid: 1,
+    noti_date: new Date(),
+    current_state: status[index],
+  };
+  io.emit("status-1", mockedData);
+  res.send("sended");
 });
+
+app.get("/ng", (req, res) => {
+  const randomedNg = Math.floor(Math.random() * 30);
+  const mockedData = {
+    mcid: 1,
+    noti_id: 1,
+    noti_date: new Date(),
+    current_state: "NG",
+    job_no: 1,
+    pp_item: 1,
+    pp_ng: randomedNg,
+    refer_id: 1,
+  };
+  io.emit("ngqty-1", mockedData);
+  res.send("sended");
+});
+
+app.get("/done", (req, res) => {
+  const random = () => Math.floor(Math.random() * 30);
+  const mockedData = {
+    mcid: random(),
+    pp_ac: random(),
+    pp_df: random(),
+    pp_ng: random(),
+    as_cycle_act: random(),
+  };
+  io.emit("doneqty-1", mockedData);
+  res.send("sended");
+});
+
+app.get("/chart", (req, res) => {
+  const status = [10, 11, 20, 30];
+
+  let mockedData = [];
+  for (let i = 1; i <= 12; i++) {
+    const plan = Math.floor(Math.random() * 100);
+    const actual = plan - Math.floor(Math.random() * 10);
+    mockedData.push({
+      Hours: i,
+      Plans: plan,
+      Actual: actual,
+    });
+  }
+  io.emit("chart-1", mockedData);
+  res.send("sended");
+});
+
+io.on("connection", (socket) => {});
 
 http.listen(9009, () => {
   console.log("http://localhost:9009");
